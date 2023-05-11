589da4b6b046e0db366f4cf60503081bfe299788
Ant <ananthajit5d@gmail.com>
2023-05-10 22:13:09 -0700

    autopytorch test here

---
+++ autopytorch_test.py
@@ -0,0 +1,47 @@
+# !pip install autopytorch
+
+import torch
+import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+import torch.nn as nn
+from torch.utils.data import DataLoader, TensorDataset
+import matplotlib.pyplot as plt
+
+device = torch.device('cpu') # change as req'd
+df = pd.read_csv('results/results_100_100000_new.csv', header=None)
+model_num = 1
+# display(df)
+results = df.to_numpy()
+print(results.shape)
+print(results[:,-1].shape)
+X = torch.tensor(results[:,:-1]).to(torch.float32).to(device)
+if model_num == 1:
+    X = torch.reshape(X,(X.shape[0],1,10,10))
+y = torch.tensor(np.expand_dims(results[:,-1], axis=1)).to(torch.float32).to(device)
+print(X.shape, y.shape)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+print(X_train.shape, y_train.shape)
+# Setting up Dataloader
+# training_dataset = TensorDataset(X_train, y_train)
+# train_loader = DataLoader(training_dataset, batch_size=24, shuffle=True)
+
+# from pytorch docs
+from autoPyTorch.api.tabular_regression import TabularRegressionTask
+
+
+api = TabularRegressionTask()
+
+api.search(
+    X_train=X_train,
+    y_train=y_train,
+    X_test=X_test.copy(),
+    y_test=y_test.copy(),
+    optimize_metric='r2',
+    total_walltime_limit=6*3600,
+    func_eval_time_limit_secs=3600)
+
+
+y_pred = api.predict(X_test)
+score = api.score(y_pred, y_test)
